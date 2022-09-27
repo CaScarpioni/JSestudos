709501c4b80a9d2e694c0c2034b22c159f6f0879
Camila Scarpioni <cascarpionic@gmail.com>
2022-09-27 18:05:40 -0300

    first commit

---
+++ aula11/composto.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+console.log(`vivendo em ${pais}`)
+if (pais=="Brasil"){
+    console.log('Brasileiro')
+}
+else{
+    console.log('Estrangeiro')
+}
---
+++ aula11/condicoes.js
@@ -0,0 +1,10 @@
+var vel = 60
+console.log('a velocidade do sue carro é ${vel} km/h')
+if (vel > 60){
+    console.log('Voce ultrapassou a velocidade')
+}
+else{
+    console.log('Você não foi multado')
+}
+
+console.log('Dirija sempre usando cinto')
